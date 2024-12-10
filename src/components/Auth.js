8361ff4cb294b5fccff9603968f5a359586a0203
ishk99 <ishratkaur121@gmail.com>
2024-12-10 09:16:22 +0530

    Updated Auth Page Buttons

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom";
-
+import styles from './Auth.module.css';
 const Auth = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className={styles.authcontainer}>
       <h2>Welcome!</h2>
       <p>Please sign up or log in to continue.</p>
       <button onClick={() => navigate("/signup")}>Signup</button>
